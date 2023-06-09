3d70d37c4c5ca297f63c27bdb5100b5133516016
Nestori <nessu.kangashaka@gmail.com>
2023-06-09 20:57:07 +0300

    Chose the color palette and now the projects area is ready

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,40 @@
 import './App.css';
 import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { TbWorldWww } from 'react-icons/tb'
 
 
 // This is a model that display project information that it gets as props.
 function Project(props)
 {
   return (
-    <div>
-
+    <div className='ProjectArea'>
+      <div className='ProjectBackground'>
+        <div className='ProjectHeaderArea'>
+          <h3>{props.name}</h3>
+        </div>
+        <div className='ProjectAboutArea'>
+          <p>{props.about}</p>
+        </div>
+      </div>
+      <div className='ProjectLinksBackground'>
+        <a href={props.github}><FaGithubSquare className='GithubIcon' /></a>
+        {props.link !== "" && <a href={props.link}><TbWorldWww className='LinkIcon' /></a>}
+      </div>
     </div>
   )
 }
 
 // This is a model that will loop through all the projects and send the individual data to the project with props.
-function Projects()
+function Projects(props)
 {
   return (
     <div className='ProjectsBackground'>
-      <h3>Project data displayed here</h3>
-      <Project />
+      {
+        props.PROJECT_DATA.map((item, index) => (
+          <Project name={item["name"]} about={item["about"]} github={item["github"]} link={item["link"]} />
+        ))
+      }
+      
     </div>
   )
 }
@@ -29,9 +45,18 @@ function App() {
   // project, I add it to this list and the project will be shown in the 'My Projects' page automatically.
   const PROJECT_DATA = [
     {
-      name: "Test name",
-      about: "Test about",
-      github: "https://github.com/Nesssu/MyPortfolio/tree/main/my-portfolio",
+      name: "My Portfolio",
+      about: `This is the website that you arecurrently in. If you want, you can inspect the source code of this project from the link below.`,
+      github: "https://github.com/Nesssu/MyPortfolio",
+      link: ""
+    },
+    {
+      name: "Advanced Web Applications Project",
+      about : `This is a project that I did for my University course called 'Advanced Web Applications'.
+      The course was a course about full-stack programming where we made front-ends with React and back-ends with Node.JS and Express.
+      In this project we had to create a smaller version of StackOverflow. You can inspect the source code from the GitHub link below.
+      There is also instructions on how to run the project on your local machine. I don't have the project running currently anywhere.`,
+      github: "https://github.com/Nesssu/AdvancedWebProject",
       link: ""
     }
   ]; 
@@ -54,7 +79,7 @@ function App() {
         <h2>My Projects</h2>
       </div>
 
-      <Projects PROJECT_DATA={{PROJECT_DATA}} />
+      <Projects PROJECT_DATA={PROJECT_DATA} />
 
       <div className='DividerHeader ImageTwo'>
         <h2>Contact Me</h2>
@@ -63,12 +88,14 @@ function App() {
       <div className='ContactArea'>
         <p className='ContactAreaText'>You can contact me at: <span className='NormalText'>nessu.kangashaka@gmail.com</span></p>
         <div className='LinkArea'>
-          <a href='https://github.com/Nesssu'><FaGithubSquare className='LinkIcon' /></a>
-          <a href='https://www.linkedin.com/in/nestori-kangashaka-230941163/'><FaLinkedin className='LinkIcon' /></a>
+          <a href='https://github.com/Nesssu'><FaGithubSquare className='GithubIcon' /></a>
+          <a href='https://www.linkedin.com/in/nestori-kangashaka-230941163/'><FaLinkedin className='LinkedinIcon' /></a>
         </div>
       </div>
 
-      <div className='Footer' />
+      <div className='Footer'>
+        <h3>Copyright 2023</h3>
+      </div>
     </div>
   );
 }
